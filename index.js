78121abd2c288c3c7e74b4694c97e97914313c03
Topper Bowers <topper@toppingdesign.com>
2017-10-11 17:15:03 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+module.exports.Constants = {
+    deviceName: "unknown javascript device",
+    deviceId: guid()
+};
+
+function guid() {
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+        s4() + '-' + s4() + s4() + s4();
+}
+
+function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+}
